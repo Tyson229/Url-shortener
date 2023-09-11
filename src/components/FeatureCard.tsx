5bb7c20df5c47eba7af3befd11e4698fa7a8f56e
Duy <s3930426@rmit.eedu.vn>
2023-09-11 07:21:40 +0700

    changees

--- src/components/FeatureCard.tsx
+++ src/components/FeatureCard.tsx
@@ -1,4 +1,5 @@
 import React from "react";
+const {BASE_URL} = import.meta.env
 
 interface FeatureCardProps {
   icon: string;
@@ -14,7 +15,7 @@ const FeatureCard: React.FC<FeatureCardProps> = ({
   return (
     <div className="h-full w-full grid bg-white p-8 rounded-md place-items-center md:place-items-start relative z-10 aspect-[1.5/1] sm:aspect-[2/0.8] md:aspect-[2/2.25] lg:aspect-[3/2] md:even:top-10 md:last:top-20">
       <div className="rounded-full p-6 bg-violet-950 absolute -top-12 md:left-7">
-        <img src={icon} alt={title} />
+        <img src={`${BASE_URL}${icon}`} alt={title} />
       </div>
       <div className="font-extrabold text-xl mt-8 mb-4">{title}</div>
       <div className="text-stone-400 text-center md:text-left">
--- src/lib/data.ts
+++ src/lib/data.ts
@@ -1,18 +1,18 @@
 export const features = [
   {
-    icon: "public/icon-brand-recognition.svg",
+    icon: "icon-brand-recognition.svg",
     title: "Brand Recognition",
     description:
       "Boost your brand recognition with each click. Generic links don't mean a thing. Branded links help instill confidence in your content.",
   },
   {
-    icon: "public/icon-detailed-records.svg",
+    icon: "icon-detailed-records.svg",
     title: "Detailed Records",
     description:
       "Gain insights into who is clicking your links. Knowing when and where people engage with your content helps inform better decisions.",
   },
   {
-    icon: "public/icon-fully-customizable.svg",
+    icon: "icon-fully-customizable.svg",
     title: "Fully Customizable",
     description:
       "Improve brand awareness and content discoverability through customizable links, supercharging audience engageme",
